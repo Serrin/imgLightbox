c3908224157d6fe72c365b07138f7adb3e7e96db
Ferenc Czigler <Serrin@users.noreply.github.com>
2020-11-18 09:58:03 +0100

    v1.0.5: new config and open method parameter (newwindow) + JS fixes + added u87.min.css v0.9.18
    v1.0.5:
     - new config and open method parameter (newwindow) 
     - JS fixes
     - added u87.min.css v0.9.18

--- imglightbox.js
+++ imglightbox.js
@@ -1,8 +1,8 @@
 /**
  * imglightbox
- * @version 1.0.4
+ * @version 1.0.5
  * @author Ferenc Czigler <serrin.salamander@gmail.com>
- * @see https://github.com/Serrin/
+ * @see https://github.com/Serrin/imgLightbox
  *
  * <link href="imglightbox.css" rel="stylesheet" type="text/css">
  * <script src="imglightbox.js"></script>
@@ -12,21 +12,32 @@ var imgLightbox = (function(){
   "use strict";
   return {
     config: {
-      version: "1.0.4",
-      download: false
+      version: "1.0.5",
+      download: false,
+      newwindow: false
     },
-    open: function (filename, download) {
+    open: function (filename, download, newwindow) {
       if (typeof download !== "boolean") { download = this.config.download; }
+      if (typeof newwindow !== "boolean") { newwindow = this.config.newwindow; }
       var els = document.getElementById("imgLightbox").style;
       els.backgroundImage = "url('"+filename+"')";
       els.display = "block";
       var dlbtn = document.getElementById("imgLightbox-button-download");
+      var nwbtn = document.getElementById("imgLightbox-button-newwindow");
       if (download) {
         dlbtn.style.display = "inline-block";
         dlbtn.href = filename;
+        dlbtn.target = "_blank";
         dlbtn.download = filename;
       } else {
         dlbtn.style.display = "none";
+      }
+      if (newwindow) {
+        nwbtn.style.display = "inline-block";
+        nwbtn.href = filename;
+        nwbtn.target = "_blank";
+      } else {
+        nwbtn.style.display = "none";
       }
     },
     close: function () {
